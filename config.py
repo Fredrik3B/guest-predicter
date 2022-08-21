bc2dea2669d265c872690e8bb91d8cd7ed66d84a
Nikolai Borbe <nikolaiborbe@gmail.com>
2022-08-21 23:27:02 +0200

    update config

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 config = {
-    "start": 11,
-    "stop": 15
+    "lokasjon": {
+        "kikut": {
+            "start": 11,
+            "stop": 15,
+            "time_ranking": {11: 5, 12: 25, 13: 35, 14: 25, 15: 10}
+        },
+    },
 }
